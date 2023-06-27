4c675ad3ed2610feaa160e1d96733fd932222cc9
Daniel <dunanes@pm.me>
2023-06-27 16:55:56 -0400

    08 Containers - Atividade final concluída

---
+++ 08-containers/atividade-final/database/db/script.sql
@@ -0,0 +1,12 @@
+CREATE database if not exists livros;
+
+USE livros;
+
+CREATE TABLE IF NOT EXISTS livros (
+    id VARCHAR(36) PRIMARY KEY,
+    nome VARCHAR(180) NOT NULL,
+    sinopse TEXT NOT NULL,
+    isbn VARCHAR(13) NOT NULL UNIQUE,
+    autores JSON NOT NULL,
+    url_imagem TEXT
+);
